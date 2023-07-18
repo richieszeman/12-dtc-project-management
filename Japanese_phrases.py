f1246fbbabaa3f03c6b72b16c64a25fe52157e13
richieszeman <124639825+richieszeman@users.noreply.github.com>
2023-07-19 11:23:04 +1200

    menu

--- Japanese_phrases.py
+++ Japanese_phrases.py
@@ -2,6 +2,6 @@
 # Japanese phrase game/quiz
 
 MENUS = ["P:)Play", "R) Reward", "S) Score", "X) quit"]
-print(Welcome to the Japanese phrases game!)
+print("Welcome to the Japanese phrases game!")
 for menu in MENUS:
     print(menu)
