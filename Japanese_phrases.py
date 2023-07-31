37cabf144197fb183f7fe7eb83ec56fde5ac444c
richieszeman <124639825+richieszeman@users.noreply.github.com>
2023-07-31 14:31:33 +1200

    menu

--- Japanese_phrases.py
+++ Japanese_phrases.py
@@ -2,6 +2,16 @@
 # Japanese phrase game/quiz
 
 MENUS = ["1) Play", "2) Reward", "3) Score", "Q) quit"]
-print("Welcome to the Japanese phrases game!")
+user_choice = input("Welcome to the Japanese phrases game!")
 for menu in MENUS:
     print(menu)
+if user_choice == 1:
+    print("playing game")
+elif user_choice == 2:
+    print("Rewards earned so far")
+elif user_choice == 3:
+    print("here is your score")
+elif user_choice == "Q":
+    print("Thank you for playing")
+else:
+    print("please select a valid choice")
