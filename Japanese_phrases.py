57b7b20b375e223ed36190c93e3f5e21866107f7
richieszeman <124639825+richieszeman@users.noreply.github.com>
2023-08-02 11:34:37 +1200

    improved

--- Japanese_phrases.py
+++ Japanese_phrases.py
@@ -5,14 +5,18 @@ MENUS = ["1) Play", "2) Reward", "3) Score", "4) quit"]
 print("Welcome to the Japanese phrases game!")
 for menu in MENUS:
     print(menu)
-user_choice = input("please pick an option: "):
-if user_choice == 1:
-    print("playing game")
-elif user_choice == 2:
-    print("Rewards earned so far")
-elif user_choice == 3:
-    print("here is your score")
-elif user_choice == "Q":
-    print("Thank you for playing")
-else:
-    print("please select a valid choice")
+
+try:
+    user_choice = int(input("please pick an option: "))
+    if user_choice == 1:
+        print("playing game")
+    elif user_choice == 2:
+        print("Rewards earned so far")
+    elif user_choice == 3:
+        print("here is your score")
+    elif user_choice == 4:
+        print("Thank you for playing")
+    else:
+        print("please select a valid choice")
+except ValueError:
+    print("please pick a valid option")
